565abbd57afcf08e2063b3488a8984a19064081d
Aymane-18 <89948033+Aymane-18@users.noreply.github.com>
2024-10-16 13:19:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,17 +6,3 @@ function toggleSection(id) {
         content.style.display = "none";
     }
 }
-
-function openModal(imageSrc, imageAlt) {
-    var modal = document.getElementById('myModal');
-    var modalImg = document.getElementById('img01');
-    var captionText = document.getElementById('caption');
-    modal.style.display = "block";
-    modalImg.src = imageSrc;
-    captionText.innerHTML = imageAlt;
-
-    var span = document.getElementsByClassName("close")[0];
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-}
