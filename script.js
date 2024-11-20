6bb708dd9766cf1ff7a5c6c87de36910de65aeb7
Aymane-18 <89948033+Aymane-18@users.noreply.github.com>
2024-11-20 18:25:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function toggleSection(id) {
     var content = document.getElementById(id);
-    if (content.style.display === "none") {
+    var button = document.querySelector(`[onclick="toggleSection('${id}')"]`);
+    if (content.style.display === "none" || content.style.display === "") {
         content.style.display = "block";
+        button.textContent = "Hide Details"; // Change button text
     } else {
         content.style.display = "none";
+        button.textContent = "Show Details"; // Revert button text
     }
 }
